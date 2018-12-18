dd716a0dbf407de712a2b6fa01a8b52a60ad11d7
Calvin Wei <cwei33333@gmail.com>
2018-12-18 13:44:05 -0800

    backed up montecarlocraps from gcloud

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from datetime import datetime, timedelta
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+@app.route('/')
+def main():
+    """Return a friendly HTTP greeting."""
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
